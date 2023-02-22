a4c8dbee5335b53175893e240fd474b57cf9c95e
Yasmeen <yasmeentayeh7@gmail.com>
2023-02-22 12:06:03 +0200

    first

--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -3,15 +3,19 @@ import {data} from "../Data";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {Search} from "../../Component/Search";
+import {FilterProduct} from "../../Component/FilterProduct";
 
 export const Products = () => {
+    const products = data?.products ;
+
     const [data , setData] = useState(null)
     const [loading , setLoading] = useState(true)
     const [error , setError] = useState(null)
     const [query , setQuery ] = useState("")
-    // const [searchParam] = useState()
+    const [filterTextValue , setFilterText] = useState("all")
+    const [newProductList , setNewProduct] = useState(products)
+    // console.log(data?.products.filter(product => product.brand.toLowerCase().includes("fa")))
 
-    console.log(data?.products.filter(product => product.brand.toLowerCase().includes("fa")))
 
     useEffect(() => {
         getData();
@@ -35,6 +39,26 @@ export const Products = () => {
             });
     }
 
+    const keys = ["brand" , "title" , "category"]
+    const search = (products) => {
+        return data?.products.filter((item) =>
+         keys.some((key) => item[key].toLowerCase().includes(query))
+        )
+    }
+
+    const FilterProductList = newProductList.filter((product) => {
+        if (filterTextValue === 'laptop') {
+            return product.category === true
+        } else {
+            return product
+        }
+
+    })
+    const onFilterValueSelected = (filterValue) => {
+        setFilterText(filterValue)
+    }
+
+
     const columns = [
         {field : "id" , header: "ID"},
         {field: "title" , header: "Title"},
@@ -45,12 +69,9 @@ export const Products = () => {
 
     return(
         <>
+            <FilterProduct filterValueSelected={onFilterValueSelected} newProductList={FilterProductList}></FilterProduct>
             <Search type="text" placeholder="name" onChange={(e)=> setQuery(e.target.value)} />
-            {data?.products.filter((product) =>
-                product.brand.toLowerCase().includes(query)
-
-            <Table key={data?.products.id} data={data?.products} columns={columns} />
-                ))}
+            <Table key={data?.products.id} data={search(data?.products)} columns={columns} />
         </>
     )
 }
